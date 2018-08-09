9eacc96be5a6ed971958dd2847dcd7cd05ffb3c9
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-08-09 23:24:58 +0900

    ・WebSocketController.cs追加。 ・簡易WebSocketシグナリングサーバーサンプルコード追加。

---
+++ Assets/Scripts/WebSocket/WebSocketController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using WebSocketSharp;
+using System.Threading;
+
+public class WebSocketController : WebRtcMsgExchanger
+{
+    [Serializable]
+    public class SignalingMessage
+    {
+        public string type;
+        public string message;
+    }
+
+    public string WebSocketServerURL = "ws://localhost:8888";
+
+    SynchronizationContext context;
+    WebSocket wsClient;
+
+    // Use this for initialization
+    void Start()
+    {
+        context = SynchronizationContext.Current;
+        wsClient = new WebSocket(WebSocketServerURL);
+        wsClient.OnOpen += (s, e) => Debug.Log("WebSocket Open");
+        wsClient.OnMessage += ReceivedMessage;
+        wsClient.OnClose += (s, e) => Debug.Log("WebSocket Close");
+        wsClient.Connect();
+    }
+
+    void ReceivedMessage(object sender, MessageEventArgs e)
+    {
+        context.Post((state) =>
+        {
+            var msg = JsonUtility.FromJson<SignalingMessage>((string)state);
+            WebRtcCtr_ReceivedMessage(msg.type, msg.message);
+        }, e.Data);
+    }
+
+    void SendMessage(string type, string message)
+    {
+        var msg = new SignalingMessage() { type = type, message = message };
+        wsClient.Send(JsonUtility.ToJson(msg));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        if (wsClient == null) return;
+        wsClient.Close();
+    }
+
+    public override void RequiredSendingMessage(string type, string message)
+    {
+        if (wsClient == null)
+        {
+            Debug.Log("WebSocket err");
+            return;
+        }
+        if (wsClient.ReadyState == WebSocketState.Open)
+            SendMessage(type, message);
+    }
+}
---
+++ WebSocketServer/index.js
@@ -0,0 +1,12 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8888 });
+wss.on('connection', ws => {
+    ws.on('message', data => {
+        console.log(data);
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        })
+    })
+})
